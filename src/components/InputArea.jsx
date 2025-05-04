8f0dd15b89fc692e8a941cce2d324f05e68f2f12
Sarthak Jain <82368183+Sarthak97Jain@users.noreply.github.com>
2025-05-04 21:00:48 +0530

    added loader on the api call delay.

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -1,12 +1,14 @@
 import React, { useState , useEffect} from 'react';
 import WeatherCard from './WeatherCard';
 import { useFetchWeatherData } from '../hooks/useFetchWeatherData';
+import { ClipLoader } from 'react-spinners';
+import './InputArea.css';
 
 const InputArea = () => {
  const [weatherData , setWeatherData] = useState(null);
  const [city , setCity] = useState('');
  const [submittedCity , setSubmittedCity] = useState(null);
- const {response , error} = useFetchWeatherData(submittedCity);
+ const {response , error, loading} = useFetchWeatherData(submittedCity);
 
  useEffect(() => {
     if (response) {
@@ -19,13 +21,39 @@ const InputArea = () => {
       setCity('');
     }
   return (
-    <>
-        <input type="text" value={city} placeholder='Type city here' onChange={(e) => setCity(e.target.value)}/>
-        <button type="submit" onClick={handleSubmit}>Get Weather</button>
-        {(!error && weatherData) && <WeatherCard weatherData = {weatherData}/>}
-        <p>{error && error}</p>
-    </>
-  )
+    <div className='weatherComponent'>
+      <input
+        type="text"
+        value={city}
+        placeholder="Type city here"
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <button type="submit" onClick={handleSubmit}>
+        Get Weather
+      </button>
+      <div className='weartherCard'>
+        {loading ? (
+          <div className='loader'>
+          <ClipLoader
+            color='white'
+            size={35}
+            aria-label="Loading Spinner"
+          />
+          </div>
+        ) : (
+          <div>
+            {!error && weatherData && <WeatherCard weatherData={weatherData} />}
+            <p>{error && error}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
+
+/////todo
+//loader
+//weather background
+//add search city
 
 export default InputArea;
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const WeatherCard = ({weatherData}) => {
   return (
-    <>
+    <div>
       <h4>{weatherData?.data?.location?.name}, {weatherData?.data?.location?.country}</h4>
       <div>Weather : {weatherData?.data?.current?.condition?.text}</div>
-      <div>Temperature : {weatherData?.data?.current?.heatindex_c}</div>
-    </>
+      <div>Temperature : {weatherData?.data?.current?.heatindex_c} C</div>
+    </div>
   )
 }
 
